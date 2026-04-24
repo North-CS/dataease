073faf168d10bf67a22e6a0ffa88a39bf548f742
xiao2man <2915374539@qq.com>
2026-04-24 16:17:50 +0800

    前端格式问题

--- core/core-frontend/src/utils/logout.ts
+++ core/core-frontend/src/utils/logout.ts
@@ -16,7 +16,7 @@ const interactiveStore = interactiveStoreWithOut()
 export const logoutHandler = (justClean?: boolean, save_platform_status = false) => {
   // alert("退出入口！")
   const ticket = getSsoTicket()
-  console.log("开始退出")
+  console.log('开始退出')
   //request.post({ url: '/sso/logout',data:{ ticket } })
   request.post({ url: '/sso/logout', data: { ticket } })
   document.cookie = `sso.jd.com=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;domain=.jd.com;`
